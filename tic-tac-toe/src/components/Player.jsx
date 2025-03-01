c79781eb242eb723d0685d9f0cd23b641bcaca45
Oleksandr Lytovka <olytovka@teramind.co>
2025-03-01 12:47:16 +0200

    add player component

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const Player = ({ name, symbol }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [newName, setNewName] = useState(name);
+    const [newSymbol, setNewSymbol] = useState(symbol);
+
+    const handleEdit = () => {
+        setIsEditing(true);
+    }
+
+    return (
+        <li>
+        <span className="player">
+            {isEditing ? 
+            <input
+                type="text"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+            />
+             : 
+            <span className="player-name">{name}</span>
+            }
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button type="button" onClick={handleEdit}>Edit</button>
+        </li>
+    );
+};
+
+export default Player;
